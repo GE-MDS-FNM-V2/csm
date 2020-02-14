c485cc9e0561fc30697ab2560280005c00aa18b1
Brady Brown <bobrown101@gmail.com>
2020-02-14 13:47:20 -0500

    fix: add axios as peer dep

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
 const path = require('path')
 
-const libraryName = "csm"
+const libraryName = 'csm'
 
-const createConfig = ({
-    libraryTarget,
-    target
-}) => {
-    return {
-        entry: './src/index.ts',
-        devtool: 'inline-source-map',
-        mode: 'production',
-        target,
-        module: {
-          rules: [
-            {
-              test: /\.tsx?$/,
-              use: 'ts-loader',
-              exclude: /node_modules/
-            }
-          ]
-        },
-        resolve: {
-          extensions: ['.tsx', '.ts', '.js']
-        },
-        output: {
-          filename: `bundle-${target}.js`,
-          path: path.resolve(__dirname, 'dist'),
-          library: libraryName,
-          libraryTarget
+const createConfig = ({ libraryTarget, target }) => {
+  return {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    mode: 'production',
+    target,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
         }
-      }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    externals: {
+      '@ge-fnm/action-object': '@ge-fnm/action-object',
+      axios: 'axios'
+    },
+    output: {
+      filename: `bundle-${target}.js`,
+      path: path.resolve(__dirname, 'dist'),
+      library: libraryName,
+      libraryTarget
+    }
+  }
 }
 
 module.exports = [
-    createConfig({libraryTarget: "umd", target: "web"}),
-    createConfig({libraryTarget: "umd", target: "node"})
+  createConfig({ libraryTarget: 'umd', target: 'web' }),
+  createConfig({ libraryTarget: 'umd', target: 'node' })
 ]
