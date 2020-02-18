50911ec94ee485ddd984aa17bd56970941be9c0e
Brady Brown <bobrown101@gmail.com>
2020-02-18 17:31:31 -0500

    fix: mark axios and action-object and peer deps (#2)
    * fix: add axios as peer dep
    
    * fix: attempt to fix peer deps
    
    * fix: attempt to fix peer dep
    
    * feat: update pam and action-object

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,49 @@
 const path = require('path')
 
-const libraryName = "csm"
+const libraryName = 'csm'
 
-const createConfig = ({
-    libraryTarget,
-    target
-}) => {
-    return {
-        entry: './src/index.ts',
-        devtool: 'inline-source-map',
-        mode: 'production',
-        target,
-        module: {
-          rules: [
-            {
-              test: /\.tsx?$/,
-              use: 'ts-loader',
-              exclude: /node_modules/
-            }
-          ]
-        },
-        resolve: {
-          extensions: ['.tsx', '.ts', '.js']
-        },
-        output: {
-          filename: `bundle-${target}.js`,
-          path: path.resolve(__dirname, 'dist'),
-          library: libraryName,
-          libraryTarget
+const createConfig = ({ libraryTarget, target }) => {
+  return {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    mode: 'production',
+    target,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
         }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    externals: {
+      '@ge-fnm/action-object': {
+        commonjs2: '@ge-fnm/action-object',
+        commonjs: '@ge-fnm/action-object',
+        amd: '@ge-fnm/action-object',
+        root: '@ge-fnm/action-object' // indicates global variable
+      },
+      axios: {
+        commonjs2: 'axios',
+        commonjs: 'axios',
+        amd: 'axios',
+        root: 'axios' // indicates global variable
       }
+    },
+    output: {
+      filename: `bundle-${target}.js`,
+      path: path.resolve(__dirname, 'dist'),
+      library: libraryName,
+      libraryTarget
+    }
+  }
 }
 
 module.exports = [
-    createConfig({libraryTarget: "umd", target: "web"}),
-    createConfig({libraryTarget: "umd", target: "node"})
+  createConfig({ libraryTarget: 'umd', target: 'web' }),
+  createConfig({ libraryTarget: 'umd', target: 'node' })
 ]
