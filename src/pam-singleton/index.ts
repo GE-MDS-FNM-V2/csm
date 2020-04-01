86e31d994da98e676ea546d33ef6569cf33ab0e3
Daniel Jones <danieljones@Daniels-MBP-3.rochester.rr.com>
2020-04-01 15:45:17 -0400

    docs: code comment refresh

--- src/pam-singleton/index.ts
+++ src/pam-singleton/index.ts
@@ -3,6 +3,16 @@ import { Executer } from '@ge-fnm/perform-action-module'
 
 const log = debug('ge-fnm:csm:pam-singleton')
 
+/**
+ * This is a singleton class so there is only ever one instance of the PAM
+ * Executer.
+ *
+ * The PAM Executer class stores session state after sessions with radios are
+ * initialized. If we did not keep the Executer reference as a singleton,
+ * then every import statement of CSM across a single application would
+ * have separate Executer class instances. This would mean they wouldn't share
+ * references to the same set of radio login sessions.
+ */
 export class LocalExecuter {
   private static instance: Executer
 
--- src/remote-csm/index.ts
+++ src/remote-csm/index.ts
@@ -3,17 +3,29 @@ import axios from 'axios'
 
 const log = debug('ge-fnm:csm:remote-csm')
 
+/**
+ * This is a stateless function that makes a POST request to a remote hosted
+ * instance of CSM, as specified by the forwardingAddress parameter, and returns
+ * the data from the remotely executed communications.
+ *
+ * This function is essential for using a reverse proxy for radio
+ * communications. There is potential for some radio communication protocols,
+ * specifically JSON-RPC, to be enabled to execute from a browser environment
+ * in the future. There are some communication methods, such as serial, that
+ * will always need to be executed from a Node environment. This reverse
+ * proxy capability enables both of those scenarios.
+ */
 export const executeRemoteAction = (
   serializedActionObject: string,
   forwardingAddress: string
 ): Promise<string> => {
-  // returning a promise there will be some sort of axios response
   log(
     'Remote executing action at',
     forwardingAddress,
     'with following action object',
     serializedActionObject
   )
+  // returning a promise there will be some sort of axios response
   return new Promise((resolve, reject) => {
     axios
       .post(forwardingAddress, { serializedAction: serializedActionObject })
