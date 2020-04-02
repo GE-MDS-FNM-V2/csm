46b9d6217ab02c21c7bac70235ca789a71f72202
Daniel Jones <danieljones@Daniels-MBP-3.rochester.rr.com>
2020-04-02 15:48:50 -0400

    docs: updating code comments to adhere to TypeDoc format

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,11 @@
 import { CommunicationMethodV1 } from '@ge-fnm/action-object'
 
 /**
+ * NOTE TO DEVELOPERS:
  * If the browser is ever capable of making calls to the radio, put the methods here.
- * As is the browser is unable to make any type of calls to the radio, including JSON-RPC
- * calls with HTTP, due to CORS issues. We want to leave this here in case some protocols are
- * unblocked in the future.
+ * In the current state of the project, the browser is unable to make any type of calls to
+ * the radio, including JSON-RPC calls with HTTP, due to CORS issues. We want to leave this
+ * here in case some protocols are unblocked in the future.
  */
 export const BROWSER_ENABLED_COMM_METHODS: CommunicationMethodV1[] = []
 export const NEEDS_FORWARDING_ADDRESS_ERROR =
